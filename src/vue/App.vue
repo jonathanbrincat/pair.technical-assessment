cd7c941b317acb4ba66fa64ecbc42fedb871f3b9
Jonathan Brincat <jonathan@pix8.co.uk>
2025-12-08 14:25:15 +0000

    sanity check my reactivity

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -11,6 +11,12 @@ onMounted(async () => {
 
   await store.fetchSessions()
 })
+
+function testReactivityHandler() {
+  const first = store.sessionCollection[0]
+  if (!first) return
+  first.completed = !first.completed
+}
 </script>
 
 <template>
@@ -25,4 +31,18 @@ onMounted(async () => {
       @toggle-complete="(id, isComplete) => store.updateStatus(id, isComplete)"
     />
   </main>
+
+  <footer class="bg-black text-white fixed bottom-0 right-0 p-4">
+    <div class="flex items-center justify-end gap-4">
+      <p>Debug:</p>
+
+      <!--JB: this was something I added to validate the reactivity was behaving as expected and have left it in situ -->
+      <button
+        class="border-3 border-blue-600 rounded hover:border-white text-blue-700 hover:text-white focus:outline-2 focus:outline-offset-1 focus:outline-blue-600 active:bg-blue-300 px-3 py-1 cursor-pointer"
+        @click="testReactivityHandler"
+      >
+        Reactivity Test (toggle completed)
+      </button>
+    </div>
+  </footer>
 </template>
