bddb600d5286b8f6e0af3494966cb781e3f68d2d
Jonathan Brincat <jonathan@pix8.co.uk>
2025-12-08 13:31:51 +0000

    some light styling to the list

--- src/vue/components/SessionItem.vue
+++ src/vue/components/SessionItem.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 defineProps<{
-  item: Record<string, string | number>
+  item: Record<string, undefined | string | number | string[]>
 }>()
 </script>
 
 <template>
-  <article>{{ item }}</article>
+  <article class="border-3 rounded-sm p-3" :aria-label="item.title">
+    <h2 class="text-lg">{{ item.title }}</h2>
+    <p><span class="text-xs font-semibold uppercase mr-2">Tags:</span>{{ item.tags }}</p>
+    <p><span class="text-xs font-semibold uppercase mr-2">Minutes:</span>{{ item.mins }}</p>
+    <p>
+      <span class="text-xs font-semibold uppercase mr-2">Difficulty:</span>{{ item.difficulty }}
+    </p>
+    <p>
+      <span class="text-xs font-semibold uppercase mr-2">Popularity:</span>{{ item.popularity }}
+    </p>
+    <p><span class="text-xs font-semibold uppercase mr-2">Status:</span>{{ item.completed }}</p>
+  </article>
 </template>
